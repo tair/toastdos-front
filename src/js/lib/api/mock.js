f29f30b29eff4ebc887a00f081ea2af087170a09
Jean Cheng <Mochi@jeans-mbp.wireless.rit.edu>
2016-11-18 00:08:07 -0500

    JSON mock object added to mock.js (getUserInfo)

--- src/js/lib/api/mock.js
+++ src/js/lib/api/mock.js
@@ -1,13 +1,32 @@
 "use strict";
 
+//run this in terminal... USE_MOCK_API=true npm start
+//check index.js (this is why this command works)
+
+var userJSONObj={
+    	"user_name": "John Doe",
+		"user_orcid_id": "1234-5678-9876-4321",
+		"email":"JoDo@do.com",
+		"roles" : [
+		    {
+		        "curator":"true"
+		    },
+		    {
+		        "researcher":"null" 
+		    },
+		    {
+		    	"viewer":"null" 
+		    }
+		]
+    };
+
 export function login(orcidCode, callback) {
     return callback(null, {
         jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcl9pZCI6NSwidXNlcl9uYW1lIjoiSm9obiBEb2UiLCJ1c2VyX29yY2lkX2lkIjoiMTIzNC01Njc4LTk4NzYtNDMyMSJ9.N2irm4JuggkMRLQRQIt9TMKKNq99AcTU8x1SBlid-nk"
     });
 }
 
+//make JSON object
 export function getUserInfo(id, callback) {
-    return callback(null, {
-        //todo mock user info response
-    });
+    return callback(null, userJSONObj);
 }
