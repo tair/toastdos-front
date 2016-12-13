09a216e34fa3cb86c6dc580e6022de483294aaef
Joseph DeSimpliciis <jcdesimp@gmail.com>
2016-12-13 01:02:52 -0500

    Created skeleton Submission view component.

---
+++ src/js/components/views/submissionView/connectedSubmissionView.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import { connect } from 'react-redux';
+import SubmissionView from './submissionView';
+
+const ConnectedSubmissionView = connect(
+    () => {},
+    () => {}
+)(SubmissionView);
+
+export default ConnectedSubmissionView;
