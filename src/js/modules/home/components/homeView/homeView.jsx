2ffaef1dc1e0f7c3639bdf027a5a3e1f57d45af1
Joseph DeSimpliciis <jcdesimp@gmail.com>
2017-05-03 12:03:16 -0400

    Add banner image and info panels to home page.

--- src/js/modules/home/components/homeView/homeView.jsx
+++ src/js/modules/home/components/homeView/homeView.jsx
@@ -1,6 +1,9 @@
 "use strict";
 
 import React from 'react';
+import atImg from 'img/at.jpg';
+import phoenixLogo from 'img/phoenixLogo.png';
+import githubLogo from 'img/githubLogo.png';
 
 class HomeView extends React.Component {
     constructor(props) {
@@ -12,9 +15,63 @@ class HomeView extends React.Component {
     }
 
     render() {
+
+        const bannerStyle = {
+            backgroundImage: `url(${atImg})`,
+        };
+
         return (
-            <div>
-                <h1>Welcome Home!</h1>
+            <div className="home">
+                <div className="banner-image" style={bannerStyle}>
+                </div>
+                <div className="info-panel-group">
+                    <div className="panel-container">
+                        <div className="panel">
+                            <div className="panel-content">
+                                <img
+                                    src={phoenixLogo}
+                                    alt="phoeniz bioinformatics logo"
+                                    style={{height: '118px'}}
+                                />
+                                <p>
+                                    ToastDos is a service from Phoenix Bioinformatics.
+                                    Visit our website to learn more about us.
+                                    <br/>
+                                </p>
+                                <a
+                                    className="btn btn-primary"
+                                    href="http://phoenixbioinformatics.org"
+                                    target="_blank"
+                                >
+                                    Visit Us
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="panel-container">
+                        <div className="panel github">
+                            <div className="panel-content">
+                                <img
+                                    src={githubLogo}
+                                    alt="github-logo"
+                                    style={{height: '100px'}}
+                                />
+                                <p>
+                                    ToastDos is open source! Contribute to this and other Tair projects on our GitHub page!
+                                </p>
+                                <a
+                                    className="btn btn-primary"
+                                    href="https://github.com/tair"
+                                    target="_blank"
+                                >
+                                    Check Out Our GitHub
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>  
         );
     }
