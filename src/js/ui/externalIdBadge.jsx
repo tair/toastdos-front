8af55d15e352cc5ddaee99354b4e8b07a2c8c5a5
Gavin Nishizawa <ghn6069@rit.edu>
2018-02-12 13:00:11 -0500

    Add PO and ECO external links #251

--- src/js/ui/externalIdBadge.jsx
+++ src/js/ui/externalIdBadge.jsx
@@ -11,19 +11,48 @@ class ExternalIdBadge extends React.Component {
         this.externalId = this.props.externalId;
     }
 
+    generateExternalIdLink(externalId) {
+        let link = <span>{externalId}</span>;
+        let baseUrl = '';
+        if (externalId.indexOf("GO:") !== -1) {
+            let goBaseUrl = 'http://amigo.geneontology.org/amigo/term/';
+            link = (
+                <a href={`${goBaseUrl}${externalId}#display-lineage-tab`}
+                    className="text-light"
+                    target="_blank"
+                >
+                    {externalId} <span className="fa fa-external-link"></span>
+                </a>
+            );
+        } else if (externalId.indexOf("PO:") !== -1) {
+            let poBaseUrl = 'http://browser.planteome.org/amigo/term/';
+            link = (
+                <a href={`${poBaseUrl}${externalId}`}
+                    className="text-light"
+                    target="_blank"
+                >
+                    {externalId} <span className="fa fa-external-link"></span>
+                </a>
+            );
+        } else if (externalId.indexOf("ECO:") !== -1) {
+            let ecoBaseUrl = 'http://www.evidenceontology.org/browse/';
+            link = (
+                <a href={`${ecoBaseUrl}#${externalId}`}
+                    className="text-light"
+                    target="_blank"
+                >
+                    {externalId} <span className="fa fa-external-link"></span>
+                </a>
+            );
+        }
+        return link;
+    }
+
     render() {
         return (
             this.externalId !== "" ?
             <Badge color="success">
-            { this.externalId.indexOf("GO:") !== -1 ?
-                <a href={`http://amigo.geneontology.org/amigo/term/${this.externalId}#display-lineage-tab`}
-                    className="text-light"
-                    target="_blank"
-                >
-                    {this.externalId} <span className="fa fa-external-link"></span>
-                </a>
-                : <span>{this.externalId}</span>
-            }
+                {this.generateExternalIdLink(this.externalId)}
             </Badge>
             :
             <Badge color="warning">
